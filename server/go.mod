5308e2c5b7436bbe8ce9c69b049815984890a764
kekeli <kekelidompeh@gmail.com>
2024-10-04 15:52:53 +0000

    feat: generate token

--- server/go.mod
+++ server/go.mod
@@ -4,6 +4,7 @@ go 1.23.0
 
 require (
 	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/yeqown/go-qrcode/v2 v2.2.4 // indirect
--- server/internal/generator.go
+++ server/internal/generator.go
@@ -2,7 +2,9 @@ package internal
 
 import (
 	"log"
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/yeqown/go-qrcode/v2"
 	"github.com/yeqown/go-qrcode/writer/standard"
 )
@@ -13,7 +15,7 @@ type Payload struct {
 	Token         string   `json:"token"`
 }
 
-func Generator(payloadData []byte) {
+func QRCGenerator(payloadData []byte) {
 	qrc, err := qrcode.New(string(payloadData))
 	if err != nil {
 		log.Fatalf("Failed to create qrcode: %v", err)
@@ -30,4 +32,25 @@ func Generator(payloadData []byte) {
 
 	log.Printf("QrCode generated successfully")
 
+}
+
+var secretkey = []byte("secret")
+
+func generateToken(payload map[string]interface{}) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	for key, value := range payload {
+		claims[key] = value
+	}
+
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	tokenString, err := token.SignedString(secretkey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
 }
